b2d868506e23b3138bbab5fa26d0d5e8ec1770fc
Thiago <thiago.miranda@sambatech.com.br>
2016-02-26 18:43:49 -0300

    uploadimagem

--- TesteMobileIOS/ViewController.swift
+++ TesteMobileIOS/ViewController.swift
@@ -9,24 +9,33 @@
 import UIKit
 import MobilePlayer
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var playerContainer: UIView!
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var titleNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    //@IBOutlet weak var playerContainer: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let videoURL = NSURL(string:  "http://gbbrfd.sambavideos.sambatech.com/account/37/2/2015-11-05/video/cb7a5d7441741d8bcb29abc6521d9a85/marina_360p.mp4")!
+        //let videoURL = NSURL(string:  "http://gbbrfd.sambavideos.sambatech.com/account/37/2/2015-11-05/video/cb7a5d7441741d8bcb29abc6521d9a85/marina_360p.mp4")!
 
-        let playerVC = MobilePlayerViewController(contentURL: videoURL)
+        //let playerVC = MobilePlayerViewController(contentURL: videoURL)
         
-        playerVC.title = "Teste Mobile"
-        playerVC.activityItems = [videoURL]
-        presentMoviePlayerViewControllerAnimated(playerVC)
+        //playerVC.title = "Teste Mobile"
+        //playerVC.activityItems = [videoURL]
+        //presentMoviePlayerViewControllerAnimated(playerVC)
 
         
-        self.playerContainer.addSubview(playerVC.view)
+        //self.playerContainer.addSubview(playerVC.view)
+        
+        
+        nameTextField.delegate = self
         
     }
 
@@ -34,7 +43,46 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        titleNameLabel.text = textField.text
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion:nil)
+    }
+    
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        photoImageView.image = selectedImage
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    // MARK: actions
+    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .PhotoLibrary
+        
+        imagePickerController.delegate = self
+        
+        presentViewController(imagePickerController, animated: true, completion: nil)
+    }
 
+    @IBAction func changeText(sender: UITextField, forEvent event: UIEvent) {
+        print("teste")
+    }
 
 }
 
